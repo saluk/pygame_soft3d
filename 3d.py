32d49de5e5a5743a3ef61c45e28874de98f79165
saluk <saluk64007@gmail.com>
2010-10-20 12:04:31 -0700

    purge old methods

--- 3d.py
+++ 3d.py
@@ -126,49 +126,7 @@ quads = [quad,quad2,quad3,quad4]
 odepth = [1000 for i in range(s_w*s_h)]
 pygame.depth = odepth[:]
 
-def draw_point(x,y,z,u,v,color=None):
-    pygame.points += 1
-    #z = 0 to 1
-    #want z = 0 to 400
-    #z/z`=140/20
-    #z = 140/20*z`
-    z = float((z*1.0/300)+1)
-    if z<=0:
-        return
-    #x = 0 to s_w
-    #x = 0 to r_w
-    x = (x*s_w/float(r_w))/z+s_w//2
-    y = (y*s_w/float(r_w))/z+s_h//2
-    x,y = int(x),int(y)
-    if x<0 or x>=s_w or y<0 or y>=s_h:
-        return
-    if pygame.depth[y*s_w+x]<z:
-        return
-    pygame.depth[y*s_w+x] = z
-    if not color:
-        color = arr[int(u*(tex.get_width()-1)),int(v*(tex.get_height()-1))]
-        #~ color = [int(color[0]/z),int(color[1]/z),int(color[2]/z)]
-    pygame.arr[x,y] = color
-
-def draw_line(s,e,color):
-    line = [s,e]
-    x,y,z,u,v = line[0]
-    steps = 100
-    xs = (line[1][0]-line[0][0])/float(steps)
-    ys = (line[1][1]-line[0][1])/float(steps)
-    zs = (line[1][2]-line[0][2])/float(steps)
-    us = (line[1][3]-line[0][3])/float(steps)
-    vs = (line[1][4]-line[0][4])/float(steps)
-    for i in range(steps):
-        draw_point(x,y,z,u,v)
-        x+=xs
-        y+=ys
-        z+=zs
-        u+=us
-        v+=vs
-
-grey = numpy.array([100,100,100],dtype=numpy.uint8)
-def draw_point2(x,y,z,u,v,texture):
+def draw_point(x,y,z,u,v,texture):
     #pygame.points += 1
     if x<0 or x>=s_w:
         return
@@ -180,118 +138,8 @@ def draw_point2(x,y,z,u,v,texture):
         return
     pygame.depth[y*s_w+x] = z
     pygame.arr[x,y] = texture[int(z*10)][int(u%1*tw),int(v%1*th)]
-    
-def draw_hline(x1,y1,z1,u1,v1,x2,y2,z2,u2,v2):
-    x,y,z,u,v = x1,y1,z1,u1,v1
-    w = abs(x2-x1)
-    if not w:
-        return
-    dx = (x2-x1)/w
-    dy = 0
-    dz = (z2-z1)/w
-    du = (u2-u1)/w
-    dv = 0
-    while 1:
-        draw_point2(int(x),int(y),z,u,v)
-        x+=dx
-        z+=dz
-        u+=du
-        if x2>x1 and x>x2:
-            break
-        if x2<x1 and x<x2:
-            break
-            
-def draw_tri_bottom(a,b,c):
-    """draw triangle"""
-    y = min([a[1],b[1],c[1]])
-    x,y,z,u,v = a
-    w = abs(b[0]-a[0])
-    va = "x"
-    if w==0:
-        w = abs(b[1]-a[1])
-        va = "y"
-    if w==0:
-        return
-    dx = (b[0]-a[0])/w
-    dy = (b[1]-a[1])/w
-    dz = (b[2]-a[2])/w
-    du = (b[3]-a[3])/w
-    dv = (b[4]-a[4])/w
-    while 1:
-        if va=="x":
-            if dx>0 and x>=b[0]:
-                break
-            if dx<0 and x<=b[0]:
-                break
-        if va=="y":
-            if dy>0 and y>=b[1]:
-                break
-            if dy<0 and y<=b[1]:
-                break
-        draw_line2(x,y,z,u,v,*c)
-        x+=dx
-        y+=dy
-        z+=dz
-        u+=du
-        v+=dv
-
-def draw_line2(x1,y1,z1,u1,v1,x2,y2,z2,u2,v2):
-    x,y,z,u,v = x1,y1,z1,u1,v1
-    w = abs(x2-x1)
-    if not w:
-        return
-    dx = (x2-x1)/w
-    dy = (y2-y1)/w
-    dz = (z2-z1)/w
-    du = (u2-u1)/w
-    dv = (v2-v1)/w
-    while 1:
-        draw_point2(int(x),int(y),z,u,v)
-        x+=dx
-        y+=dy
-        z+=dz
-        u+=du
-        v+=dv
-        if x2>x1 and x>x2:
-            break
-        if x2<x1 and x<x2:
-            break
-            
-def draw_tri2(a,b,c):
-    """draw triangle with diagonal lines from one point to
-    the two other points"""
-    x,y,z,u,v = a
-    w = abs(b[0]-a[0])
-    va = "x"
-    if w==0:
-        w = abs(b[1]-a[1])
-        va = "y"
-    if w==0:
-        return
-    dx = (b[0]-a[0])/w
-    dy = (b[1]-a[1])/w
-    dz = (b[2]-a[2])/w
-    du = (b[3]-a[3])/w
-    dv = (b[4]-a[4])/w
-    while 1:
-        if va=="x":
-            if dx>0 and x>=b[0]:
-                break
-            if dx<0 and x<=b[0]:
-                break
-        if va=="y":
-            if dy>0 and y>=b[1]:
-                break
-            if dy<0 and y<=b[1]:
-                break
-        draw_line2(x,y,z,u,v,*c)
-        x+=dx
-        y+=dy
-        z+=dz
-        u+=du
-        v+=dv
         
-def draw_tri3(a,b,c,texture):
+def draw_tri(a,b,c,texture):
     """draws triangle with horizontal lines"""
     #Sort points vertically
     a,b,c = sorted([a,b,c],key=lambda t: t[1])
@@ -337,7 +185,7 @@ def draw_tri_split(a,b,c,texture):
     draw_tri_point_up(a,b,d,texture)
     draw_tri_point_down(b,d,c,texture)
     
-def draw_line3(x1,y1,z1,u1,v1,x2,y2,z2,u2,v2,texture):
+def draw_line(x1,y1,z1,u1,v1,x2,y2,z2,u2,v2,texture):
     """horizontal"""
     x,y,z,u,v = x1,y1,z1,u1,v1
     w = abs(x2-x1)
@@ -354,7 +202,7 @@ def draw_line3(x1,y1,z1,u1,v1,x2,y2,z2,u2,v2,texture):
         if x>=s_w:
             return
         if x>=0:
-            draw_point2(int(x),int(y),z,u,v,texture)
+            draw_point(int(x),int(y),z,u,v,texture)
         x+=dx
         y+=dy
         z+=dz
@@ -380,7 +228,7 @@ def draw_tri_point_up(a,b,c,texture):
     dv1 = (b[4]-v)/ydist
     dv2 = (c[4]-ev)/ydist
     while y<=b[1]:
-        draw_line3(x,y,z,u,v,ex,ey,ez,eu,ev,texture)
+        draw_line(x,y,z,u,v,ex,ey,ez,eu,ev,texture)
         x+=dx1
         y+=dy1
         z+=dz1
@@ -410,7 +258,7 @@ def draw_tri_point_down(a,b,c,texture):
     dv1 = (c[4]-v)/ydist
     dv2 = (c[4]-ev)/ydist
     while y<=c[1]:
-        draw_line3(x,y,z,u,v,ex,ey,ez,eu,ev,texture)
+        draw_line(x,y,z,u,v,ex,ey,ez,eu,ev,texture)
         x+=dx1
         y+=dy1
         z+=dz1
@@ -422,61 +270,18 @@ def draw_tri_point_down(a,b,c,texture):
         eu+=du2
         ev+=dv2
         
-def draw_quad2(q):
+def draw_quad(q):
     """Draws a quad sample in screen space"""
     q.calc_corners()
     ul = q.corners[0]
     ur = q.corners[1]
     br = q.corners[2]
     bl = q.corners[3]
-    draw_tri3(ul,ur,br,q.texture)
-    draw_tri3(ul,br,bl,q.texture)
-    
-
-def draw_quad1(q):
-    """Draws a quad by sampling in polygon space across and down"""
-    color = q.color
-    #march start from q[0] to q[1]
-    #march end from q[3] to q[2]
-    x1 = q[0][0]
-    y1 = q[0][1]
-    z1 = q[0][2]
-    u1 = q[0][3]
-    v1 = q[0][4]
-    x2 = q[3][0]
-    y2 = q[3][1]
-    z2 = q[3][2]
-    u2 = q[3][3]
-    v2 = q[3][4]
-    xw1 = q[1][0]-q[0][0]
-    yw1 = q[1][1]-q[0][1]
-    zw1 = q[1][2]-q[0][2]
-    uw1 = q[1][3]-q[0][3]
-    vw1 = q[1][4]-q[0][4]
-    xw2 = q[2][0]-q[3][0]
-    yw2 = q[2][1]-q[3][1]
-    zw2 = q[2][2]-q[3][2]
-    uw2 = q[2][3]-q[3][3]
-    vw2 = q[2][4]-q[3][4]
-    w = float(max([abs(x) for x in [xw1,zw1,xw2,zw2]]))
-    #sample based on average distance
-    az = ((q[0][2]+q[1][2]+q[2][2]+q[3][2])/140.0+2)
-    if az<=0:
-        return
-    d1 = [x/w for x in [xw1,yw1,zw1,uw1,vw1]]
-    d2 = [x/w for x in [xw2,yw2,zw2,uw2,vw2]]
-    p1 = [x1,y1,z1,u1,v1]
-    p2 = [x2,y2,z2,u2,v2]
-    while w>0:
-        draw_line(p1,p2,color)
-        w-=1
-        for i in range(5):
-            p1[i]+=d1[i]
-            p2[i]+=d2[i]
+    draw_tri(ul,ur,br,q.texture)
+    draw_tri(ul,br,bl,q.texture)
 
 def main():
     next_update = 1
-    draw_quad = draw_quad2
     running = 1
     while running:
         dt = clock.tick(200)
